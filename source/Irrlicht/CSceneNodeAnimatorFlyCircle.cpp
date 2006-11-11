27b6600c48c1a32a818bf6e1273f6118b552f32d
irrlicht <irrlicht@dfc29bdd-3216-0410-991c-e03cc46cb475>
2006-11-11 14:54:50 +0000

    make fly circle animator serialization compatible with irrlicht 1.1
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code@294 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/CSceneNodeAnimatorFlyCircle.cpp
+++ source/Irrlicht/CSceneNodeAnimatorFlyCircle.cpp
@@ -61,6 +61,11 @@ void CSceneNodeAnimatorFlyCircle::deserializeAttributes(io::IAttributes* in, io:
 	Radius = in->getAttributeAsFloat("Radius");
 	Speed = in->getAttributeAsFloat("Speed");
 	Direction = in->getAttributeAsVector3d("Direction");
+	
+	if (Direction.equals(core::vector3df(0,0,0)))
+		Direction.set(0,1,0); // irrlicht 1.1 backwards compatibility
+	else
+		Direction.normalize();
 }
 
 
