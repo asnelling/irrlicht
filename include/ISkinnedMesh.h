25ca84f818491c266d80ef29d78a1032969de48c
lukeph <lukeph@dfc29bdd-3216-0410-991c-e03cc46cb475>
2007-06-22 09:27:49 +0000

    Made the mesh loader interface in SkinnedMesh external, so mesh loaders outside the engine can use it. (I forgot to do this before)
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/branches/SkinnedMesh@727 dfc29bdd-3216-0410-991c-e03cc46cb475

--- include/ISkinnedMesh.h
+++ include/ISkinnedMesh.h
@@ -76,6 +76,323 @@ namespace scene
 
 		virtual void convertMeshToTangents() = 0;
 
+
+
+
+
+
+
+
+		//! A mesh buffer able to choose between
+		//! S3DVertex2TCoords, S3DVertex and S3DVertexTangents at runtime
+		struct SSkinMeshBuffer : public IMeshBuffer
+		{
+			SSkinMeshBuffer()
+			{
+				#ifdef _DEBUG
+				setDebugName("SSkinMeshBuffer");
+				#endif
+			}
+
+			~SSkinMeshBuffer() {};
+
+			virtual const video::SMaterial& getMaterial() const
+			{
+				return Material;
+			}
+
+			virtual video::SMaterial& getMaterial()
+			{
+				return Material;
+			}
+
+			virtual video::S3DVertex *getVertex(u32 index)
+			{
+				switch (VertexType)
+				{
+					case video::EVT_2TCOORDS: 	return (video::S3DVertex*)&Vertices_2TCoords[index];
+					case video::EVT_TANGENTS: 	return (video::S3DVertex*)&Vertices_Tangents[index];
+					default: 					return &Vertices_Standard[index];
+				}
+			}
+
+			virtual const void* getVertices() const
+			{
+				switch (VertexType)
+				{
+					case video::EVT_2TCOORDS: 	return Vertices_2TCoords.const_pointer();
+					case video::EVT_TANGENTS: 	return Vertices_Tangents.const_pointer();
+					default: 					return Vertices_Standard.const_pointer();
+				}
+			}
+
+			virtual void* getVertices()
+			{
+				switch (VertexType)
+				{
+					case video::EVT_2TCOORDS: 	return Vertices_2TCoords.pointer();
+					case video::EVT_TANGENTS: 	return Vertices_Tangents.pointer();
+					default: 					return Vertices_Standard.pointer();
+				}
+			}
+
+			virtual u32 getVertexCount() const
+			{
+				switch (VertexType)
+				{
+					case video::EVT_2TCOORDS: 	return Vertices_2TCoords.size();
+					case video::EVT_TANGENTS: 	return Vertices_Tangents.size();
+					default: 					return Vertices_Standard.size();
+				}
+			}
+
+			virtual const u16* getIndices() const
+			{
+				return Indices.const_pointer();
+			}
+
+			virtual u16* getIndices()
+			{
+				return Indices.pointer();
+			}
+
+			virtual u32 getIndexCount() const
+			{
+				return Indices.size();
+			}
+
+			virtual const core::aabbox3d<f32>& getBoundingBox() const
+			{
+				return BoundingBox;
+			}
+
+			virtual void setBoundingBox( const core::aabbox3df& box)
+			{
+				BoundingBox = box;
+			}
+
+			virtual void recalculateBoundingBox()
+			{
+				switch (VertexType)
+				{
+					case video::EVT_STANDARD:
+					{
+						if (Vertices_Standard.empty())
+							BoundingBox.reset(0,0,0);
+						else
+						{
+							BoundingBox.reset(Vertices_Standard[0].Pos);
+							for (u32 i=1; i<Vertices_Standard.size(); ++i)
+								BoundingBox.addInternalPoint(Vertices_Standard[i].Pos);
+						}
+					}
+					case video::EVT_2TCOORDS:
+					{
+						if (Vertices_2TCoords.empty())
+							BoundingBox.reset(0,0,0);
+						else
+						{
+							BoundingBox.reset(Vertices_2TCoords[0].Pos);
+							for (u32 i=1; i<Vertices_2TCoords.size(); ++i)
+								BoundingBox.addInternalPoint(Vertices_2TCoords[i].Pos);
+						}
+					}
+					case video::EVT_TANGENTS:
+					{
+						if (Vertices_Tangents.empty())
+							BoundingBox.reset(0,0,0);
+						else
+						{
+							BoundingBox.reset(Vertices_Tangents[0].Pos);
+							for (u32 i=1; i<Vertices_Tangents.size(); ++i)
+								BoundingBox.addInternalPoint(Vertices_Tangents[i].Pos);
+						}
+					}
+				}
+			}
+
+			virtual video::E_VERTEX_TYPE getVertexType() const
+			{
+				return VertexType;
+			}
+
+			//! returns the byte size (stride, pitch) of the vertex
+			virtual u32 getVertexPitch() const
+			{
+				switch (VertexType)
+				{
+					case video::EVT_2TCOORDS: 	return sizeof(video::S3DVertex2TCoords);
+					case video::EVT_TANGENTS: 	return sizeof(video::S3DVertexTangents);
+					default: 					return sizeof(video::S3DVertex);
+				}
+			}
+
+			virtual void MoveTo_2TCoords()
+			{
+				if (VertexType==video::EVT_STANDARD)
+				{
+					for(u32 n=0;n<Vertices_Standard.size();++n)
+					{
+						video::S3DVertex2TCoords Vertex;
+						Vertex.Color=Vertices_Standard[n].Color;
+						Vertex.Pos=Vertices_Standard[n].Pos;
+						Vertex.Normal=Vertices_Standard[n].Normal;
+						Vertex.TCoords=Vertices_Standard[n].TCoords;
+						Vertices_2TCoords.push_back(Vertex);
+					}
+					Vertices_Standard.clear();
+					VertexType=video::EVT_2TCOORDS;
+				}
+			}
+
+			virtual void MoveTo_Tangents()
+			{
+				if (VertexType==video::EVT_STANDARD)
+				{
+					for(u32 n=0;n<Vertices_Standard.size();++n)
+					{
+						video::S3DVertexTangents Vertex;
+						Vertex.Color=Vertices_Standard[n].Color;
+						Vertex.Pos=Vertices_Standard[n].Pos;
+						Vertex.Normal=Vertices_Standard[n].Normal;
+						Vertex.TCoords=Vertices_Standard[n].TCoords;
+						Vertices_Tangents.push_back(Vertex);
+					}
+					Vertices_Standard.clear();
+					VertexType=video::EVT_TANGENTS;
+				}
+				else if (VertexType==video::EVT_2TCOORDS)
+				{
+					for(u32 n=0;n<Vertices_2TCoords.size();++n)
+					{
+						video::S3DVertexTangents Vertex;
+						Vertex.Color=Vertices_2TCoords[n].Color;
+						Vertex.Pos=Vertices_2TCoords[n].Pos;
+						Vertex.Normal=Vertices_2TCoords[n].Normal;
+						Vertex.TCoords=Vertices_2TCoords[n].TCoords;
+						Vertices_Tangents.push_back(Vertex);
+					}
+					Vertices_2TCoords.clear();
+					VertexType=video::EVT_TANGENTS;
+				}
+			}
+
+			video::SMaterial Material;
+			video::E_VERTEX_TYPE VertexType;
+			core::array<video::S3DVertexTangents> Vertices_Tangents;
+			core::array<video::S3DVertex2TCoords> Vertices_2TCoords;
+			core::array<video::S3DVertex> Vertices_Standard;
+			core::array<u16> Indices;
+			core::aabbox3d<f32> BoundingBox;
+		};
+
+		//! A vertex weight
+		struct SWeight
+		{
+			//! Index of the mesh buffer
+			u16 buffer_id; //I doubt 32bits is needed
+
+			//! Index of the vertex
+			u32 vertex_id; //Store global ID here
+
+			//! Weight Strength/Percentage (0-1)
+			f32 strength;
+
+			//! internal, please do not use
+			bool *_Moved;
+			core::vector3df _StaticPos;
+			core::vector3df _StaticNormal;
+		};
+
+
+		//! Animation keyframe which describes a new position, scale or rotation
+		struct SPositionKey
+		{
+			f32 frame;
+			core::vector3df position;
+		};
+
+		struct SScaleKey
+		{
+			f32 frame;
+			core::vector3df scale;
+		};
+
+		struct SRotationKey
+		{
+			f32 frame;
+			core::quaternion rotation;
+		};
+
+		//! Joints
+	  	struct SJoint
+		{
+			//! The name of this joint
+			core::stringc Name;
+
+			//! Local matrix of this joint
+			core::matrix4 LocalMatrix;
+
+			//! List of child joints
+			core::array<SJoint*> Children;
+
+			//! Animation keys causing translation change
+			core::array<SPositionKey> PositionKeys;
+
+			//! Animation keys causing scale change
+			core::array<SScaleKey> ScaleKeys;
+
+			//! Animation keys causing rotation change
+			core::array<SRotationKey> RotationKeys;
+
+			//! Skin weights
+			core::array<SWeight> Weights;
+
+			//! Internal, please do not use
+			core::matrix4 _GlobalMatrix;
+			core::matrix4 _GlobalInversedMatrix;
+			core::vector3df _Animatedposition;
+			core::vector3df _Animatedscale;
+			core::quaternion _Animatedrotation;
+			core::matrix4 _GlobalAnimatedMatrix;
+			core::matrix4 _LocalAnimatedMatrix;
+			bool _LocalAnimatedMatrix_Animated;
+		};
+
+
+
+
+
+
+		//Interface for the mesh loaders (finalize should lock these functions, and they should have some prefix like loader_
+
+		//these functions will use the needed arrays, set vaules, etc to help the loaders
+
+		//! exposed for loaders to add mesh buffers
+		virtual core::array<SSkinMeshBuffer*> &getMeshBuffers() = 0;
+
+		//! alternative method for adding joints
+		virtual core::array<SJoint*> &getAllJoints() = 0;
+
+		//! loaders should call this after populating the mesh
+		virtual void finalize() = 0;
+
+
+
+
+		virtual SSkinMeshBuffer *createBuffer() = 0;
+
+		virtual SJoint *createJoint(SJoint *parent=0) = 0;
+
+		virtual SPositionKey *createPositionKey(SJoint *joint) = 0;
+		virtual SScaleKey *createScaleKey(SJoint *joint) = 0;
+		virtual SRotationKey *createRotationKey(SJoint *joint) = 0;
+
+		virtual SWeight *createWeight(SJoint *joint) = 0;
+
+
+
+
 	};
 
 } // end namespace scene
