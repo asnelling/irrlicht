844541e46b2edc30454f64d9abdb3948ac7f7b99
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2008-03-19 21:48:23 +0000

    Fixed the missing Windows API check for _WIN32 and _WIN64, both in trunk and 1.4. Finished the API enhancements in quaternion and vector classes. All methods now return the object where necessary.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/branches/releases@1297 dfc29bdd-3216-0410-991c-e03cc46cb475

--- 1.4/include/IrrCompileConfig.h
+++ 1.4/include/IrrCompileConfig.h
@@ -29,7 +29,7 @@
 //! WIN32 for Windows32
 //! WIN64 for Windows64
 // The windows platform and API support SDL and WINDOW device
-#if defined(WIN32) || defined(WIN64)
+#if defined(_WIN32) || defined(_WIN64) || defined(WIN32) || defined(WIN64)
 #define _IRR_WINDOWS_
 #define _IRR_WINDOWS_API_
 #ifndef _IRR_USE_SDL_DEVICE_
