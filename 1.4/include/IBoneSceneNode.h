0c4e608ee0010178d24df6ff3f1362c47f35c015
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2008-05-15 12:33:26 +0000

    Added missing copyright headers.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/branches/releases@1347 dfc29bdd-3216-0410-991c-e03cc46cb475

--- 1.4/include/IBoneSceneNode.h
+++ 1.4/include/IBoneSceneNode.h
@@ -1,3 +1,7 @@
+// Copyright (C) 2002-2008 Nikolaus Gebhardt
+// This file is part of the "Irrlicht Engine".
+// For conditions of distribution and use, see copyright notice in irrlicht.h
+
 #ifndef __I_BONE_SCENE_NODE_H_INCLUDED__
 #define __I_BONE_SCENE_NODE_H_INCLUDED__
 
