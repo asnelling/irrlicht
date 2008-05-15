ac3a761c201543aeda0fb3debd10bbce79d6642f
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2008-05-15 12:33:26 +0000

    Added missing copyright headers.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/branches/releases/1.4@1347 dfc29bdd-3216-0410-991c-e03cc46cb475

--- include/IGUISpriteBank.h
+++ include/IGUISpriteBank.h
@@ -1,3 +1,6 @@
+// Copyright (C) 2002-2008 Nikolaus Gebhardt
+// This file is part of the "Irrlicht Engine".
+// For conditions of distribution and use, see copyright notice in irrlicht.h
 
 #ifndef __I_GUI_SPRITE_BANK_H_INCLUDED__
 #define __I_GUI_SPRITE_BANK_H_INCLUDED__
