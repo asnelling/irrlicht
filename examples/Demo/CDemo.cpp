33e7589f2a7da23ee1eed3d374442d792c17373b
irrlicht <irrlicht@dfc29bdd-3216-0410-991c-e03cc46cb475>
2008-06-01 05:52:57 +0000

    some view adjustments for release 1.4.1 (codeblocks project update, demo settings update
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/branches/releases/1.4@1358 dfc29bdd-3216-0410-991c-e03cc46cb475

--- examples/Demo/CDemo.cpp
+++ examples/Demo/CDemo.cpp
@@ -731,7 +731,7 @@ void CDemo::createParticleImpacts()
 				if (sound)
 				{
 					// adjust max value a bit to make to sound of an impact louder
-					sound->setMinDistance(200);
+					sound->setMinDistance(400);
 					sound->drop();
 				}
 			}
