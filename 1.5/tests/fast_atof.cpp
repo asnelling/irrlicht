add9b8febde0fc2157591b6e7b55e3c27fab785a
Rogerborg <Rogerborg@dfc29bdd-3216-0410-991c-e03cc46cb475>
2008-11-28 12:21:50 +0000

    Add some basic automated tests to 1.5.  Windows / MSVC project files only at the moment, other platforms/compilers to be added as and when, in addition to control images for other specific driver version numbers (or squelch the explicit versioning).  Add a new creation method to plane3d, which is actually tested! :)
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/branches/releases@1857 dfc29bdd-3216-0410-991c-e03cc46cb475

---
+++ 1.5/tests/fast_atof.cpp
@@ -0,0 +1,171 @@
+// Test the accuracy and speed of 
+
+#include "irrlicht.h"
+#include <assert.h>
+#include <float.h>
+#include <limits.h>
+#include <windows.h>
+
+using namespace irr;
+using namespace core;
+
+static inline u32 old_strtol10(const char* in, const char** out=0)
+{
+	u32 value = 0;
+
+	while ( ( *in >= '0') && ( *in <= '9' ))
+	{
+		value = ( value * 10 ) + ( *in - '0' );
+		++in;
+	}
+	if (out)
+		*out = in;
+	return value;
+}
+
+//! Provides a fast function for converting a string into a float,
+//! about 6 times faster than atof in win32.
+// If you find any bugs, please send them to me, niko (at) irrlicht3d.org.
+static inline const char* old_fast_atof_move( const char* c, float& out)
+{
+	bool inv = false;
+	const char *t;
+	float f;
+
+	if (*c=='-')
+	{
+		++c;
+		inv = true;
+	}
+
+	//f = (float)strtol(c, &t, 10);
+	f = (float) old_strtol10 ( c, &c );
+
+	if (*c == '.')
+	{
+		++c;
+
+		//float pl = (float)strtol(c, &t, 10);
+		float pl = (float) old_strtol10 ( c, &t );
+		pl *= fast_atof_table[t-c];
+
+		f += pl;
+
+		c = t;
+
+		if (*c == 'e')
+		{
+			++c;
+			//float exp = (float)strtol(c, &t, 10);
+			bool einv = (*c=='-');
+			if (einv)
+				++c;
+
+			float exp = (float)old_strtol10(c, &c);
+			if (einv)
+				exp *= -1.0f;
+
+			f *= (float)pow(10.0f, exp);
+		}
+	}
+
+	if (inv)
+		f *= -1.0f;
+
+	out = f;
+	return c;
+}
+
+static inline float old_fast_atof(const char* c)
+{
+	float ret;
+	old_fast_atof_move(c, ret);
+	return ret;
+}
+
+
+static bool testCalculation(const char * valueString)
+{
+	const f32 newFastValue = fast_atof(valueString);
+	const f32 oldFastValue = old_fast_atof(valueString);
+	const f32 atofValue = (f32)atof(valueString);
+
+	(void)printf("\n String '%s'\n New fast %.40f\n Old fast %.40f\n     atof %.40f\n",
+		valueString, newFastValue, oldFastValue, atofValue);
+
+	bool accurate = fabs(newFastValue - atofValue) <= fabs(oldFastValue - atofValue);
+
+	if(!accurate)
+		(void)printf("*** ERROR - less accurate than old method ***\n\n");
+
+	return accurate;
+}
+
+bool fast_atof(void)
+{
+	bool accurate = true;
+
+	accurate &= testCalculation("340282346638528859811704183484516925440.000000");
+	accurate &= testCalculation("3.402823466e+38F");
+	accurate &= testCalculation("3402823466e+29F");
+	accurate &= testCalculation("-340282346638528859811704183484516925440.000000");
+	accurate &= testCalculation("-3.402823466e+38F");
+	accurate &= testCalculation("-3402823466e+29F");
+	accurate &= testCalculation("34028234663852885981170418348451692544.000000");
+	accurate &= testCalculation("3.402823466e+37F");
+	accurate &= testCalculation("3402823466e+28F");
+	accurate &= testCalculation("-34028234663852885981170418348451692544.000000");
+	accurate &= testCalculation("-3.402823466e+37F");
+	accurate &= testCalculation("-3402823466e+28F");
+	accurate &= testCalculation(".00234567");
+	accurate &= testCalculation("-.00234567");
+	accurate &= testCalculation("0.00234567");
+	accurate &= testCalculation("-0.00234567");
+	accurate &= testCalculation("1.175494351e-38F");
+	accurate &= testCalculation("1175494351e-47F");
+	accurate &= testCalculation("1.175494351e-37F");
+	accurate &= testCalculation("1.175494351e-36F");
+	accurate &= testCalculation("-1.175494351e-36F");
+	accurate &= testCalculation("123456.789");
+	accurate &= testCalculation("-123456.789");
+	accurate &= testCalculation("0000123456.789");
+	accurate &= testCalculation("-0000123456.789");
+
+	if(!accurate)
+	{
+		(void)printf("Calculation is not accurate, so the speed is irrelevant\n");
+		return false;
+	}
+
+	enum { ITERATIONS = 1000000 };
+	int i;
+	
+	f32 value;
+	u32 then = GetTickCount();
+	for(i = 0; i < ITERATIONS; ++i)
+		value = (f32)atof("-340282346638528859811704183484516925440.000000");
+
+	const u32 atofTime = GetTickCount() - then;
+
+	then += atofTime;
+	for(i = 0; i < ITERATIONS; ++i)
+		value = fast_atof("-340282346638528859811704183484516925440.000000");
+	const u32 fastAtofTime = GetTickCount() - then;
+
+	then += fastAtofTime;
+	for(i = 0; i < ITERATIONS; ++i)
+		value = old_fast_atof("-340282346638528859811704183484516925440.000000");
+	const u32 oldFastAtofTime = GetTickCount() - then;
+
+	(void)printf("         atof time = %d\n    fast_atof Time = %d\nold fast_atof time = %d\n",
+		atofTime, fastAtofTime, oldFastAtofTime);
+
+	if(fastAtofTime * 3 > atofTime)
+	{
+		(void)printf("The fast method isn't at least three times as fast as atof()\n");
+		return false;
+	}
+
+	return true;
+}
+
---
+++ 1.5/tests/main.cpp
@@ -0,0 +1,35 @@
+// This is the entry point for the Irrlicht test suite.
+#include <stdio.h>
+
+// This is an MSVC pragma to link against the Irrlicht library.
+// Other builds must link against it in the project files.
+#if defined(_MSC_VER)
+#pragma comment(lib, "Irrlicht.lib")
+#endif // _MSC_VER
+
+/* Each test must have the same signature.  Test should (but are not
+ * required to) live in a .cpp file of the same name.  There is no 
+ * need to #include anything since the test entry points can be
+ * declared as extern before calling them.
+ */
+#define RUN_TEST(testEntryPoint)\
+	extern bool testEntryPoint(void);\
+	if(!testEntryPoint()) fails++;
+
+//! This is the main entry point for the Irrlicht test suite.
+/** \return The number of test that failed, i.e. 0 is success. */
+int main()
+{
+	int fails = 0;
+
+	RUN_TEST(planeMatrix);
+	RUN_TEST(screenshots);
+	RUN_TEST(disambiguateTextures);
+	RUN_TEST(drawPicsel);
+	RUN_TEST(fast_atof);
+	RUN_TEST(line2dIntersectWith);
+
+	(void)printf("\n\nTests finished. %d test failed.\n", fails);
+
+	return fails;
+}
