0cea00a09b2a8f20727238b6f0692cc24a37344b
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2009-11-10 16:25:54 +0000

    Ensure a clean state reset in the driver when scene manager clears or redraws.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/branches/releases/1.5@2832 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/CSceneManager.cpp
+++ source/Irrlicht/CSceneManager.cpp
@@ -1255,6 +1255,7 @@ void CSceneManager::drawAll()
 	video::IVideoDriver* driver = getVideoDriver();
 	if ( driver )
 	{
+		driver->setMaterial(video::SMaterial());
 		driver->setTransform ( video::ETS_PROJECTION, core::IdentityMatrix );
 		driver->setTransform ( video::ETS_VIEW, core::IdentityMatrix );
 		driver->setTransform ( video::ETS_WORLD, core::IdentityMatrix );
@@ -1676,6 +1677,9 @@ void CSceneManager::removeAll()
 {
 	ISceneNode::removeAll();
 	setActiveCamera(0);
+	// Make sure the driver is reset, might need a more complex method at some point
+	if (Driver)
+		Driver->setMaterial(video::SMaterial());
 }
 
 
