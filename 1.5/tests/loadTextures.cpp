82cd4e04c59eec41b76eaa2ee6fe5078728e5d7a
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2009-12-12 14:28:29 +0000

    Fix test to reflect the actual (and for this version acceptable) behavior.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/branches/releases@3045 dfc29bdd-3216-0410-991c-e03cc46cb475

--- 1.5/tests/loadTextures.cpp
+++ 1.5/tests/loadTextures.cpp
@@ -15,7 +15,7 @@ using namespace gui;
 bool loadFromFileFolder(void)
 {
 	IrrlichtDevice *device =
-		createDevice( video::EDT_NULL, dimension2d<s32>(640, 480));
+		createDevice( video::EDT_NULL, dimension2di(160, 120));
 
 	if (!device)
 	{
@@ -25,38 +25,71 @@ bool loadFromFileFolder(void)
 
 	IVideoDriver * driver = device->getVideoDriver();
 
+	u32 numTexs = driver->getTextureCount();
+
 	ITexture * tex1 = driver->getTexture("../media/tools.png");
 	assert(tex1);
 	if(!tex1)
 		logTestString("Unable to open ../media/tools.png\n");
+	if (driver->getTextureCount()!=numTexs+1)
+	{
+		logTestString("No additional texture in the texture cache");
+		return false;
+	}
 
 	IReadFile * readFile = device->getFileSystem()->createAndOpenFile("../media/tools.png");
 	assert(readFile);
 	if(!readFile)
 		logTestString("Unable to open ../media/tools.png\n");
+	if (driver->getTextureCount()!=numTexs+1)
+	{
+		logTestString("Additional texture in the texture cache (1)\n");
+		return false;
+	}
 
 	ITexture * tex2 = driver->getTexture(readFile);
 	assert(tex2);
 	if(!readFile)
 		logTestString("Unable to create texture from ../media/tools.png\n");
+	if (driver->getTextureCount()!=numTexs+1)
+	{
+		logTestString("Additional texture in the texture cache (2)\n");
+		return false;
+	}
 
 	readFile->drop();
 
 	// adding  a folder archive
 	device->getFileSystem()->addFolderFileArchive( "../media/" );
+
 	ITexture * tex3 = driver->getTexture("tools.png");
 	assert(tex3);
 	if(!tex3)
 		logTestString("Unable to open tools.png\n");
+	if (driver->getTextureCount()!=numTexs+2)
+	{
+		logTestString("No additional texture in the texture cache (3)\n");
+		return false;
+	}
+
+	ITexture * tex4 = driver->getTexture("tools.png");
+	assert(tex4);
+	if(!tex4)
+		logTestString("Unable to open tools.png\n");
+	if (driver->getTextureCount()!=numTexs+2)
+	{
+		logTestString("Additional texture in the texture cache (4)\n");
+		return false;
+	}
 
 	device->drop();
-	return (tex1 == tex2 && tex1 == tex3);
+	// in 1.5 we create duplicates in file folders.
+	return ((tex1 == tex2) && (tex3 == tex4));
 }
 
 bool loadTextures()
 {
 	bool result = true;
-	result |= loadFromFileFolder();
+	result &= loadFromFileFolder();
 	return result;
 }
-
